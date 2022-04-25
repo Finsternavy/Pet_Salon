e6b840367a6012aa932644f0967c7b381bb69aa7
Christopher Finster <finsternavy@gmail.com>
2022-04-25 18:47:43 -0500

    registration complete

--- scripts/display.js
+++ scripts/display.js
@@ -16,8 +16,10 @@ function displayCards(){
     for(let i = 0; i<petSalon.pets.length; i++){
         let pet = petSalon.pets[i];
         tmp +=`
-            <div class="pet-info">
-                <h4>${pet.name}</h4>
+            <div id="${pet.id}" class="pet-info">
+                <div class="pet-name-header">
+                    <h4>${pet.name}</h4><button onclick="deletePet(${pet.id});" class="delete-button"><i class="fa-solid fa-delete-left"></i></button>
+                </div>
                 <div class="pet-info-labels">
                     <label>Age: ${pet.age}</label>
                     <label>Gender: ${pet.gender}</label>
@@ -25,6 +27,7 @@ function displayCards(){
                     <label>Service: ${pet.service}</label>
                     <label>Owner: ${pet.ownerName}</label>
                     <label>Phone: ${pet.contactNum}</label>
+                    <label>Phone: ${pet.rewards}</label>
                 </div>
             </div>
         `;
@@ -34,7 +37,7 @@ function displayCards(){
 // display the pets in a table
 
 function displayTable(){
-    let tr = `
+    let tableData = `
         <th>Name</th>
         <th>Age</th>
         <th>Gender</th>
@@ -42,12 +45,13 @@ function displayTable(){
         <th>Service</th>
         <th>Owner</th>
         <th>Phone</th>
+        <th>Rewards?</th>
     `;
 
     for(let i = 0; i<petSalon.pets.length; i++){
         let pet = petSalon.pets[i];
-        tr +=`
-            <tr>
+        tableData +=`
+            <tr id="${pet.id}">
                 <td>${pet.name}</td>
                 <td>${pet.age}</td>
                 <td>${pet.gender}</td>
@@ -55,11 +59,11 @@ function displayTable(){
                 <td>${pet.service}</td>
                 <td>${pet.ownerName}</td>
                 <td>${pet.contactNum}</td>
+                <td>${pet.rewards}</td>
             </tr>
         `;
     }
-    document.getElementById("register-table").innerHTML = tr;
-    console.log(tr);
+    document.getElementById("register-table").innerHTML = tableData;
 }
 
 
--- scripts/register.js
+++ scripts/register.js
@@ -38,9 +38,9 @@ function displayPetsNames(){
 }
 displayPetsNames();
 
-// need to make this to catch up
+let c = 0; // counter for id
 class Pet {
-    constructor(name, age, gender, breed, service, ownerName, contactNum) {
+    constructor(name, age, gender, breed, service, ownerName, contactNum, rewards) {
         this.name = name;
         this.age = age;
         this.gender = gender;
@@ -48,6 +48,8 @@ class Pet {
         this.service = service;
         this.ownerName = ownerName;
         this.contactNum = contactNum;
+        this.rewards = rewards;
+        this.id = c++;
     }
 }
 
@@ -61,23 +63,24 @@ function register(){
     document.getElementById("pet-breed").value,
     document.getElementById("service").value,
     document.getElementById("owner-name").value,
-    document.getElementById("contact-phone").value
+    document.getElementById("contact-phone").value,
+    document.getElementById("rewards").value
     );
 
     if(isValid(pet)){
         petSalon.pets.push(pet);
         console.log(petSalon.pets);
         //displayPet();
-        //displayCards();
+        displayCards();
         displayTable();
         clearInputs();
         return;
     }
 }
 
-let pet1 = new Pet("Sparkles", 4, "M","dog", "Wash", "Chris", "555-555-5555");
-let pet2 = new Pet("Lotto", 4, "F", "dog", "Wash", "Chris", "555-555-5555");
-let pet3 = new Pet("Pups", 4, "M", "dog", "Wash", "Chris", "555-555-5555");
+let pet1 = new Pet("Sparkles", 4, "M","dog", "Wash", "Chris", "555-555-5555", "No");
+let pet2 = new Pet("Lotto", 4, "F", "dog", "Wash", "Chris", "555-555-5555", "Yes");
+let pet3 = new Pet("Pups", 4, "M", "dog", "Wash", "Chris", "555-555-5555", "No");
 petSalon.pets.push(pet1, pet2, pet3);
 
 function isValid(aPet){
@@ -87,30 +90,41 @@ function isValid(aPet){
     if(aPet.name.length == 0){
         valid=false;
         alert("Invalid name. Name cannot be empty!");
+        return;
     }
     if(aPet.age.length == 0){
         valid=false;
         alert("Invalid age. Age cannot be empty!");
+        return;
     }
     if(aPet.gender.length == 0){
         valid=false;
         alert("Invalid gender. Gender cannot be empty!");
+        return;
     }
     if(aPet.breed.length == 0){
         valid=false;
         alert("Invalid breed. Breed cannot be empty!");
+        return;
     }
     if(aPet.service.length == 0){
         valid=false;
         alert("Please select a service!");
+        return;
     }
     if(aPet.ownerName.length == 0){
         valid=false;
         alert("Invalid owner name. Owner name cannot be empty!");
+        return;
     }
     if(aPet.contactNum.length == 0){
         valid=false;
         alert("Invalid contact number. Please enter a contact number!");
+        return;
+    }
+    if(aPet.rewards.length == 0){
+        valid=false;
+        alert("Please elect to enroll in rewards or decline.")
     }
 
     return valid;// true or fasle
@@ -126,6 +140,68 @@ function clearInputs(){
     document.getElementById("service").value = "";
     document.getElementById("owner-name").value = "";
     document.getElementById("contact-phone").value = "";
+    document.getElementById("rewards").value = "";
+}
+
+function deletePet(petId){
+    console.log("Deleting.." + petId);
+    // in this function ------------
+        // travel the array
+        for(let i = 0; i<petSalon.pets.length; i++){
+            let pet = petSalon.pets[i];
+            if(petId == pet.id){
+                let deleteIndex = i;
+                petSalon.pets.splice(deleteIndex, 1);
+            }
+        }
+        document.getElementById(petId).remove();
+        displayCards();
+}
+
+function searchPet(){
+    
+    let search = document.getElementById("search-box").value;
+    document.getElementById("search-box").value="";
+    search = search.toUpperCase();
+
+    console.log("Searching: " + search);
+
+    for(let i = 0; i < petSalon.pets.length; i++){
+        let pet = petSalon.pets[i];
+        
+        if(search === pet.name.toUpperCase() ||
+            search == pet.age ||
+            search === pet.gender.toUpperCase() ||
+            search === pet.breed.toUpperCase() ||
+            search === pet.service.toUpperCase() ||
+            search === pet.ownerName.toUpperCase() ||
+            search === pet.contactNum.toUpperCase()){
+                console.log("Found it");
+                document.getElementById(pet.id).classList.add("highlight");
+                // get all td's in the table
+                let tds = document.querySelectorAll("#register-table td");
+                // search for the desired text in each td inner html
+                for(let i = 0; i < tds.length; i++){
+                    if(tds[i].innerHTML.toUpperCase() == search){
+                        console.log(tds[i].innerHTML);
+                        // add the highlight class to that td
+                         tds[i].classList.add("highlight");
+                    }else{
+                        // remove the highlight class from cells that do not match the search
+                        tds[i].classList.remove("highlight");
+                    }
+                }
+        }else{
+            document.getElementById(pet.id).classList.remove("highlight");
+            // remove old selections in the table.  this is a little glitchy.
+            let tds = document.querySelectorAll("#register-table td");
+                for(let i = 0; i < tds.length; i++){
+                    if(tds[i].innerHTML.toUpperCase() !== search){
+                         tds[i].classList.remove("highlight");
+                    }
+                }
+        }
+    }
 }
 
 displayCards();
