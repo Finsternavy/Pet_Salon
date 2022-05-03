9f004c5a0555219a6ab27f43ff378eb69a1d7ed5
Christopher Finster <finsternavy@gmail.com>
2022-05-03 00:02:32 -0500

    update service cards

---
+++ scripts/services.js
@@ -0,0 +1,20 @@
+// let imgs = [];
+
+// imgs[0] = new Image();
+// imgs[0].src = "img/dog_wash.jpg";
+
+// imgs[1] = new Image();
+// imgs[1].src = "img/cat_pedicure.jpg";
+
+// imgs[2] = new Image();
+// imgs[2].src = "img/dog_haricut.jpg";
+
+// imgs[3] = new Image();
+// imgs[3].src = "img/pet_massage.jpg";
+
+// imgs[4] = new Image();
+// imgs[4].src = "img/pet_breeding.jpg";
+
+// function displayServiceCards(){
+
+// }
